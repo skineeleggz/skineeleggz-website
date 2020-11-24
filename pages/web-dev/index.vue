80dbedfbb369105254be6e43911b602afed52526
skineeleggz <skineeleggz@gmail.com>
2020-11-24 04:34:47 -0500

    Added sublte joke to Web Dev page

--- pages/web-dev/index.vue
+++ pages/web-dev/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h2>Web Dev Help</h2>
-    <p>In this section, I will be posting content that will help anyone start up a website using the Nuxt.js framework.</p>
+    <p>I'm not just a guy with skinny legs, I'm a programmer. In this section, I will be posting content that will help anyone who might be interested in starting their own website. Or, if you just want to get better with Nuxt.js.</p>
     <p>Click <a href="https://github.com/skineeleggz/skineeleggz-website">here</a> to check out the GitHub repo.</p>
   </div>
 </template>
